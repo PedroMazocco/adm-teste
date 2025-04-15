7c7b3daa840f4a08666e6541bae09b65fe78606b
Pedro Henrique Mazocco <160538422+PedroMazocco@users.noreply.github.com>
2025-04-15 19:45:19 -0300

    usuario
    implementado função de cadastro de usuario

---
+++ src/main/java/com/adm/adm/controller/UsuarioController.java
@@ -0,0 +1,50 @@
+package com.adm.adm.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.adm.adm.model.Usuario;
+import com.adm.adm.service.UsuarioService;
+
+import jakarta.validation.Valid;
+
+@Controller
+@RequestMapping("/usuario")
+public class UsuarioController {
+	
+	@Autowired
+	private UsuarioService service;
+	
+	@GetMapping("/novo")
+	public ModelAndView novo(Usuario usuario) {
+		ModelAndView mv = new ModelAndView("CadastroUsuario");
+		mv.addObject("usuario", usuario);
+		return mv;
+	}
+	
+	
+	@PostMapping
+	public ModelAndView cadastrar(@Valid Usuario usuario, RedirectAttributes attributes, BindingResult result) {
+		if(result.hasErrors()) {
+			return novo(usuario);
+		}
+		service.cadastrar(usuario);
+		attributes.addFlashAttribute("mensage", "Usúario salvo com sucesso!");
+		return new ModelAndView("redirect:/usuario");
+	}
+	
+	@GetMapping
+	public ModelAndView listarUsuarios() {
+		ModelAndView mv = new ModelAndView("ListaUsuarios");
+		 mv.addObject("usuarios", service.listaUsuarios());
+		return mv;
+		
+	}
+
+}
--- src/main/java/com/adm/adm/service/UsuarioService.java
+++ src/main/java/com/adm/adm/service/UsuarioService.java
@@ -1,5 +1,7 @@
 package com.adm.adm.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,5 +20,15 @@ public class UsuarioService {
         return repository.existsByEmailAndSenha(login.getEmail(), login.getSenha());
 		
 	}
+
+	public void cadastrar(@Valid Usuario usuario) {
+		repository.save(usuario);
+	}
+
+	public List<Usuario>  listaUsuarios() {
+		return repository.findAll();
+		
+	}
+
 
 }
