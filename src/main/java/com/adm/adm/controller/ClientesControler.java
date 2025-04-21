0c9058117a8389337b321a5505b7ff58e77d203e
Pedro Henrique Mazocco <160538422+PedroMazocco@users.noreply.github.com>
2025-04-21 17:16:16 -0300

    ajuste
    tirado print no console de CPF/CNPJ utilizada para teste

--- src/main/java/com/adm/adm/controller/ClientesControler.java
+++ src/main/java/com/adm/adm/controller/ClientesControler.java
@@ -79,7 +79,6 @@ public class ClientesControler {
     @GetMapping("/{codigo}")
     public ModelAndView editar(@PathVariable Long codigo) {
         Cliente cliente = service.buscarPorId(codigo);
-        System.out.println("CPF/CNPJ carregado do banco: " + cliente.getCpfOuCnpj()); 
         ModelAndView mv = new ModelAndView("CadastroCliente");
         mv.addObject("cliente", cliente);
         mv.addObject("tiposPessoa", TipoPessoa.values());
